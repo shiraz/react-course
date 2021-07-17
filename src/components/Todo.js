2f80bb89d96d151a49490682617b95b991468fdc
mirzasb <mirzasbaig@nyu.edu>
2021-07-17 00:59:57 -0400

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Todo = () => {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="actions">
+        <button className="btn">DELETE</button>
+      </div>
+    </div>
+  );
+};
